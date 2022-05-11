b2146d3443df26578ad2baa92937b016dfb19ee8
Kamyyyk <kamyk962@gmail.com>
2022-05-11 11:23:15 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onClick={function() {console.log("Kliknięto w przycisk");}}>
+            // <button className="square" onClick={function() {console.log("Kliknięto w przycisk");}}> To jest to samo co nizej
+            <button className="square" 
+            onClick = {() => {this.props.onClick();}}>
                 {this.props.value}
             </button>
         );
@@ -14,8 +16,16 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
     renderSquare(i) {
-        return <Square value ={i}/>
+        return <Square value = {this.state.squares[i]}
+        onClick={() => this.handleClick(i)} />
     }
 
     render() {
